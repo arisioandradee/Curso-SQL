bbab5a31c799136da93ea5b540319a233e0835b3
arisioandradee <arisiosaf@gmail.com>
2024-09-04 15:30:36 -0300

    Removendo arquivos antigos

--- 11. Projeto - Pizzaria do João/pizzaria_dados.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO status (nome) VALUES ('Em preparação'), ('Em entrega'), ('Concluído');
-
-INSERT INTO massas(nome) VALUES ('Massa comum'), ('Massa temperada'), ('Massa temperada');
-
-INSERT INTO bordas(nome) VALUES ('Cheddar'), ('Catupiry'), ('Chocolate');
-
-INSERT INTO sabores(nome) VALUES ('4 Queijois'), ('Calabresa'), ('Mista'), ('Frango'),
-	('Portuguesa'), ('Carne de Sol');
